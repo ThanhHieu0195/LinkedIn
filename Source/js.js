a4e4085bfd5894509a9f1f4c44b071c7b3b06fea
HieuThanh <thanhhieu0195@gmail.com>
2016-04-28 07:22:14 +0700

    v1.0

---
+++ Source/js.js
@@ -0,0 +1,48 @@
+
+angular.module('MyProfile', [])
+        .controller('ExampleController', ['$scope','$http', function($scope, $http)
+        {
+            
+            $http.get('json/header.json').success(function(data) {
+              $scope.header = data;
+            });
+            
+           $http.get('json/profile.json').success(function(data) {
+              $scope.profile = data;
+            });
+
+           $http.get('json/profile_overview.json').success(function(data) {
+              $scope.profile_overview = data;
+            });
+
+           $http.get('json/extra_info.json').success(function(data) {
+              $scope.extra_infos = data;
+            });
+
+            $http.get('json/aboutme.json').success(function(data) {
+              $scope.aboutmes = data;
+            });    
+
+            $http.get('json/summary.json').success(function(data) {
+              $scope.summary = data;
+            });     
+            
+            $http.get('json/position.json').success(function(data) {
+              $scope.positions = data;
+            });  
+            
+             $http.get('json/skill.json').success(function(data) {
+              $scope.skills = data;
+            }); 
+             $http.get('json/education.json').success(function(data) {
+              $scope.education = data;
+            });  
+             $http.get('json/project.json').success(function(data) {
+              $scope.project = data;
+            });  
+       
+           
+        }]);
+                        
+ 
+                   
